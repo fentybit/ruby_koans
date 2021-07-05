c3ea94f8807cf40037fc68ac0ecbaf21fc3a9fda
fentybit <fentymhall@gmail.com>
2021-07-04 21:26:22 -0700

    160/282

--- download/koans/triangle.rb
+++ download/koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && b == c && c == a
+    return :equilateral
+  elsif a != b && b != c && c != a
+    return :scalene
+  else 
+    return :isosceles
+  end  
 end
 
 # Error class used in part 2.  No need to change this code.
